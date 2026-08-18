e6c1c55145e7404df69a3bf089808a735142c26a
Dheeraj sharma <38658957+Dheerajaktu@users.noreply.github.com>
2026-08-18 20:33:18 +0530

    Add: longest-consecutive-sequence - submission-3

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-3.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        int n = nums.length;
+        int answer = 0;
+        HashSet<Integer> set = new HashSet<>();
+        
+        //Simply adding unique value in the SET 
+        for(int i = 0; i<n; i++) set.add(nums[i]);
+
+        /* Logical notes - Good Question
+           Pehle Set banao taaki kisi number ko quickly check kar sako.
+           Har number ko dekho: kya iska previous number (num - 1) Set mein hai?
+           Agar previous nahi hai → yahi sequence ka START hai.
+           Ab num + 1 se aage badho aur jab tak number milta rahe, length count karo.
+           Agar previous hai → skip, kyunki sequence already pehle wale number se start ho chuki hai.
+           End mein sabse badi sequence length return karo.
+        */
+
+       // Now in out set input would be like this --> [2,20,4,10,3,5], means no duplicate
+       int start = 0;
+       int longestConsutive = 0;
+       for(int num: nums){
+       /* in first attempt I made mistake I don't need to run loop on all element from input(because main input may have so many duplicate, that will not good in case of high/very high input).
+       Insted now I'm running loop on set.
+       */
+       
+          int current = num;
+          if(!set.contains(current - 1)){
+            int length =1;
+            while(set.contains(current + 1)){
+                length++;
+                current++;
+            }
+            longestConsutive = Math.max(longestConsutive, length);
+          }  
+       }
+        return longestConsutive;
+    }
+}
