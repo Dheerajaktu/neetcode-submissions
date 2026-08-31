5f5b2853890e00814399bd99e344132c11ca493e
Dheeraj sharma <38658957+Dheerajaktu@users.noreply.github.com>
2026-08-31 23:28:21 +0530

    Add: daily-temperatures - submission-2

---
+++ Data Structures & Algorithms/daily-temperatures/submission-2.java
@@ -0,0 +1,33 @@
+class Solution {
+  /*
+    
+    Pattern: Monotonic Stack
+    Har day ke liye future mein first warmer temperature find karna hai.
+    Answer = warmer day index - current day index.
+    Stack mein temperature nahi, index store karte hain.
+    Stack mein woh days rehte hain jo warmer temperature ka wait kar rahe hain.
+    Current temperature agar stack.peek() ke temperature se greater hai → us day ka answer mil gaya → pop().
+    current - previousDay = kitne days wait kiya.
+    Phir current day ko stack mein push() karo.
+    Jo indexes end tak stack mein bach gaye → unke liye warmer day nahi mila → result 0.
+    
+  */
+
+    public int[] dailyTemperatures(int[] temperatures) {
+
+        int n = temperatures.length;
+        int[] result = new int[n];
+        Deque<Integer> stack = new ArrayDeque<>();
+
+         for (int current = 0; current < n; current++) {
+            // case check if stack is not empty 
+             while(!stack.isEmpty() && temperatures[current] > temperatures[stack.peek()]){
+                int previousDay = stack.pop();
+                result[previousDay] = current - previousDay;
+             }
+             stack.push(current);
+        }
+       return result;
+    }
+    
+}
